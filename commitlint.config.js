cda6655ca4eca5bf636c6c58de3dbe4b3269e5ce
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-22 10:39:18 -0300

    chore: setup husky and commitlint for workflow automation

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'build',
+        'ci',
+      ],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 70],
+  },
+};
