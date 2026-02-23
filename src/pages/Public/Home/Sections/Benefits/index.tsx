fbacbdae894798e141cfd24248b7b0f75924c938
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-23 17:05:07 -0300

    refactor: extract benefits data into an array and render dynamically

--- src/pages/Public/Home/Sections/Benefits/index.tsx
+++ src/pages/Public/Home/Sections/Benefits/index.tsx
@@ -1,42 +1,41 @@
 import { CircleQuestionMark, IterationCw, Truck } from 'lucide-react';
 
+const benefits = [
+  {
+    icon: Truck,
+    title: 'Frete Grátis',
+    description:
+      'Para todo o Brasil acima de R$ 200,00. Exceto para a região Norte.',
+  },
+  {
+    icon: IterationCw,
+    title: 'Trocas Grátis',
+    description:
+      'Você tem até 30 dias para trocar seus produtos. Exceto para a região Norte.',
+  },
+  {
+    icon: CircleQuestionMark,
+    title: 'Atendimento 24/7',
+    description:
+      'Nossa equipe está disponível 24 horas por dia, 7 dias por semana para atender você.',
+  },
+];
+
 const Benefits = () => {
   return (
     <section className="border-b border-gray-200 py-20">
       <div className="wrapper">
         <div className="grid gap-8 md:grid-cols-3">
-          <div className="flex items-start gap-4">
-            <Truck className="text-primary" size={64} />
-            <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-semibold">Frete Grátis</h3>
-              <p className="text-base">
-                Para todo o Brasil acima de R$ 200,00. Exceto para a região
-                Norte.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex items-start gap-4">
-            <IterationCw className="text-primary" size={64} />
-            <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-semibold">Trocas Grátis</h3>
-              <p className="text-base">
-                Você tem até 30 dias para trocar seus produtos. Exceto para a
-                região Norte.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex items-start gap-4">
-            <CircleQuestionMark className="text-primary" size={64} />
-            <div className="flex flex-col gap-2">
-              <h3 className="text-2xl font-semibold">Atendimento 24/7</h3>
-              <p className="text-base">
-                Nossa equipe está disponível 24 horas por dia, 7 dias por semana
-                para atender você.
-              </p>
-            </div>
-          </div>
+          {benefits.length > 0 &&
+            benefits.map((benefit) => (
+              <div key={benefit.title} className="flex items-start gap-4">
+                <benefit.icon className="text-primary" size={64} />
+                <div className="flex flex-col gap-2">
+                  <h3 className="text-2xl font-semibold">{benefit.title}</h3>
+                  <p className="text-base">{benefit.description}</p>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </section>
