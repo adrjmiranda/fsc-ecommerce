19b8f78f806a671ebb80cb5c6bcd926ef7683a37
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-22 21:46:52 -0300

    feat: add public about, categories, and contact pages with routes

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,12 @@ import Register from '@/pages/Auth/Register';
 import NotFound from '@/pages/Error/NotFound';
 import Home from '@/pages/Public/Home/index.tsx';
 import PublicLayout from '@/pages/Public/Layout.tsx';
+import { Contact } from 'lucide-react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import About from './pages/Public/About';
+import Categories from './pages/Public/Categories';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -18,6 +22,9 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />}>
           <Route path="" element={<PublicLayout />}>
             <Route index element={<Home />} />
+            <Route path="categorias" element={<Categories />} />
+            <Route path="sobre" element={<About />} />
+            <Route path="contato" element={<Contact />} />
           </Route>
           <Route path="autenticacao" element={<AuthLayout />}>
             <Route path="login" element={<Login />} />
