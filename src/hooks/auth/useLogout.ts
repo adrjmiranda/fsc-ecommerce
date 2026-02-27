f194ab5fbc63be9c13ab1795b9312a36b62fc641
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-27 11:29:12 -0300

    fix: add logout dispatch in logout hook

--- src/hooks/auth/useLogout.ts
+++ src/hooks/auth/useLogout.ts
@@ -1,4 +1,6 @@
 import { MUTATION_KEYS } from '@/constants/mutationKeys';
+import { logout } from '@/store/auth/slice';
+import { useAppDispatch } from '@/store/hooks';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,11 +8,14 @@ import { AuthService } from '@/services/auth';
 
 export const useLogout = () => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const { mutateAsync, isPending } = useMutation({
     mutationKey: [MUTATION_KEYS.SIGN_OUT],
     mutationFn: AuthService.signOut,
     onSuccess: () => {
+      dispatch(logout());
+
       navigate('/autenticacao/login');
     },
     onError: (error: unknown) => {
