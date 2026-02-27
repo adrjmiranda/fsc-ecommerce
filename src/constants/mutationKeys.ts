df67431d9cfb5bee87b6a5293b6844063d6a85d5
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-27 11:08:33 -0300

    feat: implement usesignin hook with react query and redux integration

--- src/constants/mutationKeys.ts
+++ src/constants/mutationKeys.ts
@@ -1,5 +1,6 @@
 const MUTATION_KEYS = {
   SIGN_UP: 'sign-up',
+  SIGN_IN: 'sign-in',
 };
 
 export { MUTATION_KEYS };
--- src/hooks/auth/useSignIn.ts
+++ src/hooks/auth/useSignIn.ts
@@ -1,10 +1,21 @@
+import { MUTATION_KEYS } from '@/constants/mutationKeys';
+import { auth } from '@/lib/firebase';
+import { login } from '@/store/auth/slice';
+import { useAppDispatch } from '@/store/hooks';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import * as z from 'zod';
+
+import { AuthService } from '@/services/auth';
 
 import { signInSchema } from '@/schemas/auth';
 
 const useSignIn = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
   const form = useForm<z.infer<typeof signInSchema>>({
     resolver: zodResolver(signInSchema),
     defaultValues: {
@@ -13,13 +24,42 @@ const useSignIn = () => {
     },
   });
 
-  const onSubmit = (data: z.infer<typeof signInSchema>) => {
-    console.log(data);
+  const { mutateAsync, isPending } = useMutation({
+    mutationKey: [MUTATION_KEYS.SIGN_IN],
+    mutationFn: AuthService.signIn,
+    onSuccess: () => {
+      const user = auth.currentUser;
+
+      if (user) {
+        dispatch(
+          login({
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+          }),
+        );
+      }
+
+      form.reset();
+      navigate('/');
+    },
+    onError: (error: unknown) => {
+      console.error('Login failed: ', error);
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const signInData = {
+      email: data.email,
+      password: data.password,
+    };
+    await mutateAsync(signInData);
   };
 
   return {
     form,
     onSubmit,
+    isPending,
   };
 };
 
