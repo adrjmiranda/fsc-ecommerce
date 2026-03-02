8e694dea285027eacfddfcab5d0b352ab70abb15
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-02 09:19:40 -0300

    feat: configure vitest with react testing library and jsdom env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
